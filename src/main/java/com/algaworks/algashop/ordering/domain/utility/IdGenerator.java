a0401f560fe4fa0782dfe28bb13739390821f90d
Danilo Ferreira <danilocsf@gmail.com>
2025-12-04 15:38:21 -0300

    Order Aggregate

--- src/main/java/com/algaworks/algashop/ordering/domain/utility/IdGenerator.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/utility/IdGenerator.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.ordering.domain.utility;
 
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
+import io.hypersistence.tsid.TSID;
 
 import java.util.UUID;
 
@@ -10,10 +11,23 @@ public class IdGenerator {
     private static final TimeBasedEpochRandomGenerator timeBasedEpochRandomGenerator
             = Generators.timeBasedEpochRandomGenerator();
 
+    private static final TSID.Factory tsidFactroy = TSID.Factory.INSTANCE;
+
     private IdGenerator() {
     }
 
     public static UUID generateTimeBasedUUID() {
         return timeBasedEpochRandomGenerator.generate();
     }
+
+    /*
+     * Para funcionar corretmaente em produçõa, com diversas instâncias de um microserviço
+     * essas variáveis de ambiente precisarão serem informadas
+     * TSID_NODE
+     * TSID_NODE_COUNT
+     */
+    public static TSID gererateTSID() {
+        return tsidFactroy.generate();
+    }
+
 }
